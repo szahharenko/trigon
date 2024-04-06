06a4f0e88d6037602894a55d61b8b30a2d430c93
Sergei Zahharenko <sergei.zahharenko@riverty.com>
2024-04-06 05:51:04 +0300

    Update

--- src/web-app/App.tsx
+++ src/web-app/App.tsx
@@ -1,31 +1,12 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import './assets/web-app.scss';
-import Loading from './Loading';
 import Welcome from './Welcome';
-import { VIEWS, CONTENT, animationTime } from '../tools/types';
 
 const App: React.FC = () => {
-
-  const [currentView, setView] = useState(VIEWS.LOADING);
-  const [currentContent, setContent] = useState(CONTENT.NONE);
-  //const viewRef = useRef<HTMLDivElement>(null);
-
-  const startApp = () : void => {
-    setTimeout( () => {
-      setView(VIEWS.WELCOME);
-      setContent(CONTENT.LANDING);
-    }, animationTime + 100 );
-  }
-
   return (
     <div className='main-app'>
       <div className='app-container'>
-        { currentView === VIEWS.LOADING && <Loading onLoadComplete={startApp}></Loading> }
-        { currentView === VIEWS.WELCOME &&
-          <div className='content'>
-              { currentContent === CONTENT.LANDING && <Welcome></Welcome> }
-          </div>
-        }
+        <Welcome></Welcome>
       </div>
     </div>
   );
