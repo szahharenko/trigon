e1781692ad33ed26e1c043d7e11e76774c8e54f7
Sergei Zahharenko <Sergei.Zahharenko@forsta.com>
2024-04-09 09:49:21 +0200

    Texts

--- src/web-app/App.tsx
+++ src/web-app/App.tsx
@@ -30,27 +30,33 @@ const App: React.FC = () => {
         }}
         expires={150}
       >
-        We use cookies to collect information about you. We use this to to give you a better experience of our website.
+        Sellel veebilehel kasutatakse küpsiseid. Veebilehe kasutamist jätkates nõustute sellega. Loe ka ettevõtte <a href="#n">Privaatsuspoliitikat</a>.
       </CookieConsent>
       {
         cookies.disclaimer !== true ?
           <div className='disclaimer'>
               <div>
-		            <h1>Disclaimer</h1>
-                <p>Welcome to the Trigon Capital website. You must read the following information for your own protection and benefit. It explains the legal and regulatory restrictions which apply to any investment in the products referred to in this website. When you have read the following information click on the “I Accept”’ button to acknowledge that you have read and understood the information and to continue to the website menu. If you do not wish to proceed, please click on “I decline” and this will return you to the Homepage.</p>
-                <p>Conditions of Use:</p>
-                <p>Nothing on this web page may be considered as part of an offer of fund units or an invitation to subscribe for fund units.</p>
-                <p>By using this website you confirm that you have read, understood and accepted the following conditions. Your access to this website is governed by the version of these conditions then-in-force. All disputes relating to these conditions or your use of the website are governed by the laws of the Republic of Estonia.</p>
-                <p>1.The distribution of the information contained on this website in certain jurisdictions may be restricted by law, including but not limited to the United States or US Persons (as defined in Regulation S under the U.S. Securities Act 1933).</p>
-                <p>2. The provision of the information in this web site does not constitute an offer of securities to any person in the United States or to any U.S. Person as such term is defined under the Securities Act of 1933, as amended. The information contained in this site about Trigon Capital and/or any of its affiliates is not directed to any person in the United States. The funds described for informational purposes only herein are not offered for sale to private investors in the USA, its territories, possessions or protectorates under its jurisdiction.</p>
-                <p>3. The information contained herein does not constitute a distribution, an offer to sell, or the solicitation of an offer to buy any securities in any jurisdiction in which such distribution or offer is not authorized.</p>
-                <p>4. Nothing in this website should be construed as investment, tax, legal or other advice nor is it to be relied upon in making an investment decision. Those accessing the Trigon Capital website should consult their financial advisers regarding the suitability of any of the products referred to on this website. The value of investments and the income from them may go down as well as up and an investor may receive back less than the original investment; past performance is not necessarily a guide to future performance.</p>
-                <p>5. The website is provided to you on an “as is” basis. Trigon Capital does not guarantee that this website or any content is error-free, of satisfactory quality or suitable for any particular purpose, accurate, up-to-date, or available at all times. You are entirely responsible for your use of the website, and for the consequences of relying on any content.</p>
-                <p>6. To the maximum extent permitted by law, Trigon Capital disclaims all liability to you arising out of your use of the website. In particular, Trigon Capital shall not be liable for any direct or indirect loss or damage to you, any loss of profits, loss of business, revenue, data, goodwill, or any consequential loss or damage.</p>
-                <p>7. All content on this website is protected by copyright. Unless consent is given by Trigon Capital, no content, materials or images accessible on or via this website may be copied, reproduced, republished, uploaded, posted, transmitted or distributed in any way.</p>
+                <h1>TEADE</h1>
+                <p>Trigondividendifond.ee veebilehe kasutamiseks peate lugema järgmist teavet. See selgitab õiguslikke ja regulatiivseid piiranguid, mis kehtivad mis tahes investeeringutele sellel veebisaidil viidatud finantstoodetesse.</p>
+                <p>Kui olete järgmise teabe läbi lugenud, klõpsake nuppu "Nõustun", kinnitamaks, et olete teabe läbi lugenud ja sellest aru saanud. Kui te ei soovi jätkata, klõpsake "Keeldun".</p>
+                <h2>Veebilehe kasutustingimused:</h2>
+                <p>Sellelt veebisaidilt leiate teavet AS Trigon Asset Management valitsetava investeerimisfondi Trigon Dividendifond kohta. Mitte midagi sellel veebilehel ei tohi käsitleda fondiosakute pakkumise või fondiosakute märkimise kutsena. Seda veebisaiti kasutades kinnitate, et olete järgmised tingimused läbi lugenud, neist aru saanud ja nõustute nendega. Teie juurdepääsu sellele veebisaidile reguleerib nende tingimuste sel ajal kehtiv versioon. Kõik vaidlused, mis on seotud nende tingimuste või teie veebisaidi kasutamisega, on reguleeritud Eesti Vabariigi seadustega.</p>
+                <ul>
+                  <li>1.	Käesoleva veebilehe sisu on avaldanud AS Trigon Asset Management (registrikood 11339670; aadress Pärnu mnt 18, Tallinn 10141, Eesti).</li>
+                  <li>2.	AS Trigon Asset Management tegevusloaga eurofondi fondivalitseja ning allub Finantsinspektsiooni järelevalvele.</li>
+                  <li>3.	See veebisait, sealhulgas kogu saadaolev teave, ei ole suunatud ega mõeldud ühelegi isikule ega üksusele, kes on resident või kelle asukohariik on riigis, kus Trigon Dividendifondi kohta teabe levitamine rikuks kehtivaid seadusi või määrusi või kus AS Trigon Asset Management ei ole õigustatud fondi kohta infot avaldama.</li>
+                  <li>4.	See veebisait, sealhulgas kogu saadaolev teave, ei kujuta endast müügipakkumist ega sellel veebisaidil esitatud fondide osakute ostmise pakkumist.</li>
+                  <li>5.	Midagi sellel veebisaidil ei tohiks tõlgendada investeerimisnõustamisena või maksualase, õigusalase või muu nõuandena ning sellele ei tohi investeerimisotsuse tegemisel tugineda. Trigondividendifond.ee veebisaidile sisenejad peaksid sellel veebisaidil viidatud toodete sobivuse osas konsulteerima oma finantsnõustajatega.</li>
+                  <li>6.	Investeeringut kaaludes tuleb olla teadlik järgmistest riskidest: Investeeringute väärtus ja nendest saadav tulu võib nii langeda kui ka tõusta ning investor võib saada tagasi alginvesteeringust vähem; fondi senine tootlus ei garanteeri samasugust tootlust tulevikus.</li>
+                  <li>7.	Fondi prospektide ja muude dokumentide (nt investori põhiteabe dokumendid, majandusaasta aruanne, poolaastaaruanne) kehtivad versioonid on kättesaadavad sellel veebilehel ja soovi korral saab neid küsida AS’ilt Trigon Asset Management.</li>
+                  <li>8.	Selleks, et investeerimisotsuseid saaks teha informeeritud alusel, palume tutvuda asjakohaste fondi pakkumise alusdokumentidega. Enne investeerimist veenduge, et mõistate fondi olemust ja sellega kaasnevaid spetsiifilisi riske, et teha kindlaks, kas fond on teie jaoks sobiv toode.</li>
+                  <li>9.	AS Trigon Asset Management ei garanteeri, et see veebisait või mis tahes sisu on vigadeta, rahuldava kvaliteediga või mis tahes konkreetseks otstarbeks sobiv. Vastutate täielikult veebisaidi kasutamise ja mis tahes sisule tuginemise tagajärgede eest.</li>
+                  <li>10.	AS Trigon Asset Management loobub seadusega lubatud maksimaalses ulatuses igasugusest vastutusest teie ees, mis tuleneb teie veebisaidi kasutamisest. Eelkõige ei vastuta AS Trigon Asset Management teile investeeringu tulemusel tekkida võiva mis tahes otsese või kaudse kahju eest, mis tahes saamata jäänud kasumi või väärtuse kaotuse eest.</li>
+                  <li>11.	Kogu sellel veebisaidil olev sisu on kaitstud autoriõigusega. Kui AS Trigon Asset Management pole selleks nõusolekut andnud, ei tohi sellel veebisaidil või selle kaudu juurdepääsetavat sisu, materjale ega pilte ühelgi viisil kopeerida, reprodutseerida, uuesti avaldada, üles laadida, postitada, edastada ega levitada.</li>
+                </ul>
                 <div className="actions">
-                  <button className='button' onClick={() => setCookie('disclaimer', 'true')}>I accept</button>
-                  <a className='button' href="https://trigoncapital.com/">I decline</a>
+                  <button className='button' onClick={() => setCookie('disclaimer', 'true')}>Ok</button>
+                  <a className='button' href="https://trigoncapital.com/">Loobu</a>
                 </div>
               </div>
           </div>
